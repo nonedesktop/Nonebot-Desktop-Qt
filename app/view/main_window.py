e501356cd19c3215cd47ad741015fc66330394cd
rainzee wang <rainzee.w@gmail.com>
2023-06-16 17:05:45 +0800

    feat: initialize layout in main window
    Implemented the _init_layout() method in the main_windows.py file within the "view" package. This method initializes the layout for the main window.

--- app/view/main_window.py
+++ app/view/main_window.py
@@ -1,4 +1,5 @@
 from qframelesswindow import FramelessWindow
+from PySide6.QtWidgets import QHBoxLayout
 
 
 class MainWindow(FramelessWindow):
@@ -6,9 +7,17 @@ class MainWindow(FramelessWindow):
         super().__init__()
         self.setObjectName("MainWindow")
         self._init_windows_size()
+        self._init_layout()
 
     def _init_windows_size(self):
         self.resize(900, 700)
         # self.setMaximumSize()
         # self.setMinimumSize()
         ## Error raised as no arguments, fill args before uncommenting.
+
+    def _init_layout(self):
+        self.h_box_layout = QHBoxLayout(self)
+        self.h_box_layout.setSpacing(0)
+        self.h_box_layout.setContentsMargins(0, 0, 0, 0)
+        # TODO add addWidget
+        # self.h_box_layout.addWidget()
