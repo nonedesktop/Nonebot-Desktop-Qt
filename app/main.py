ead30967f7fb11bde55a65b40111a7e095843834
rainzee wang <rainzee.w@gmail.com>
2023-06-18 22:40:53 +0800

    refactor: use the controller to control everything
    A purer MVC paradigm, with controllers to control UI rendering and control models

--- app/main.py
+++ app/main.py
@@ -2,13 +2,12 @@ import time
 
 from PySide6.QtWidgets import QApplication
 
-from view.main_window import MainWindow
+from controller.controller import Controller
 
 if __name__ == "__main__":
     time_start = time.perf_counter()
     app = QApplication()
-    gui = MainWindow()
-    gui.show()
+    ctr = Controller()
     time_end = time.perf_counter()
     print(f"系统渲染UI耗时{((time_end - time_start) * 1000):.1f}毫秒")
     app.exec()
