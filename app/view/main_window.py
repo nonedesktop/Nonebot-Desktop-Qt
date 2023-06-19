252c532bcc71fb0ba32458cad81d66f15edb7dd3
rainzee wang <rainzee.w@gmail.com>
2023-06-19 14:28:06 +0800

    fix: adjusting the init layout position
    The layout object should be constructed after the navigation object, otherwise it will be called while the navigation object is not yet constructed, which will result in a fatal error

--- app/view/main_window.py
+++ app/view/main_window.py
@@ -8,8 +8,8 @@ class MainWindow(FramelessWindow):
         super().__init__()
         self.setObjectName("MainWindow")
         self._init_windows_size()
-        self._init_layout()
         self._init_navigation_interface()
+        self._init_layout()
 
     def _init_windows_size(self):
         self.resize(900, 700)
@@ -22,7 +22,7 @@ class MainWindow(FramelessWindow):
         self.h_box_layout.setSpacing(0)
         self.h_box_layout.setContentsMargins(0, 0, 0, 0)
         # TODO add addWidget
-        # self.h_box_layout.addWidget()
+        self.h_box_layout.addWidget(self.navigationInterface)
 
     def _init_navigation_interface(self):
         self.navigationInterface = NavigationInterface(
