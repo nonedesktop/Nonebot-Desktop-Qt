aa1b8f6905fd39a09673bc44f7c48285f8d5df21
rainzee wang <rainzee.w@gmail.com>
2023-06-27 18:40:38 +0800

    feat: set bottom menu

--- app/view/main_window.py
+++ app/view/main_window.py
@@ -1,4 +1,4 @@
-from qfluentwidgets import SplitFluentWindow
+from qfluentwidgets import SplitFluentWindow, NavigationItemPosition
 
 from view.interface import InterfaceTemplates
 
@@ -14,10 +14,15 @@ class MainWindow(SplitFluentWindow):
     def __createSubInterface(self):
         self.home_interface = InterfaceTemplates("HomeInterface", "看板子界面", self)
         self.market_interface = InterfaceTemplates("MarketInterface", "扩展商店子界面", self)
+        self.setting_interface = InterfaceTemplates("SettingInterface", "设置子界面", self)
 
     def __initNavigation(self):
+        # set top menu
         self.addSubInterface(self.home_interface, None, "DashBoard")
+        self.navigationInterface.addSeparator()
         self.addSubInterface(self.market_interface, None, "Extension Market")
+        # set bottom menu
+        self.addSubInterface(self.setting_interface, None, "Settings", NavigationItemPosition.BOTTOM)
 
     def __initWindow(self):
         self.setWindowTitle('Nonebot-Desktop-Qt')
