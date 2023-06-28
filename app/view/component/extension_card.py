3641749fe79c14525bf924f8a369361a42dd612d
worldmozara <worldmozara@163.com>
2023-06-28 19:59:59 +0800

    feat: annotate card properties

--- app/view/component/extension_card.py
+++ app/view/component/extension_card.py
@@ -2,11 +2,41 @@ from qfluentwidgets import CardWidget
 
 
 class ExtensionCard(CardWidget):
+    module_name: str
+    """Name for importing"""
+
+    project_link: str
+    """Name for downloading from PyPI"""
+
+    name: str
+    """Human-readable name"""
+
+    desc: str
+    """Description"""
+
+    author: str
+    """Author"""
+
+    homepage: str
+    """Project homepage"""
+
+    tags: list[dict[{"label": str, "color": str}]]
+    """Tags"""
+
+    is_official: bool
+    """Whether an extension is official"""
+
     def __init__(self, parent=None):
         super().__init__(parent=parent)
 
 
 class PluginCard(ExtensionCard):
+    type: str
+    """Plugin category"""
+
+    supported_adapters: list[str]
+    """Supported adapters"""
+
     def __init__(self, parent=None):
         super().__init__(parent=parent)
 
