1da9402ed835d4e9c81f6a827e64edff58cd9e12
rainzee wang <rainzee.w@gmail.com>
2023-06-28 20:50:57 +0800

    feat: annotate card properties

--- app/view/component/extension_card.py
+++ app/view/component/extension_card.py
@@ -2,45 +2,90 @@ from qfluentwidgets import CardWidget
 
 
 class ExtensionCard(CardWidget):
-    module_name: str
-    """Name for importing"""
-
-    project_link: str
-    """Name for downloading from PyPI"""
-
-    name: str
-    """Human-readable name"""
-
-    desc: str
-    """Description"""
-
-    author: str
-    """Author"""
-
-    homepage: str
-    """Project homepage"""
-
-    tags: list[dict[{"label": str, "color": str}]]
-    """Tags"""
-
-    is_official: bool
-    """Whether an extension is official"""
-
-    def __init__(self, parent=None):
+    def __init__(
+        self,
+        module_name: str,
+        project_link: str,
+        name: str,
+        desc: str,
+        author: str,
+        homepage: str,
+        tags: list[dict[{"label": str, "color": str}]],
+        is_official: bool,
+        parent=None,
+    ):
         super().__init__(parent=parent)
+        self.module_name = module_name
+        """Name for importing"""
+        self.project_link = project_link
+        """Name for downloading from PyPI"""
+        self.name = name
+        """Human-readable name"""
+        self.desc = desc
+        """Description"""
+        self.author = author
+        """Author"""
+        self.homepage = homepage
+        """Project homepage"""
+        self.tags = tags
+        """Tags"""
+        self.is_official = is_official
+        """Whether an extension is official"""
 
 
 class PluginCard(ExtensionCard):
-    type: str
-    """Plugin category"""
-
-    supported_adapters: list[str]
-    """Supported adapters"""
-
-    def __init__(self, parent=None):
-        super().__init__(parent=parent)
+    def __init__(
+        self,
+        module_name: str,
+        project_link: str,
+        name: str,
+        desc: str,
+        author: str,
+        homepage: str,
+        tags: list[dict[{"label": str, "color": str}]],
+        is_official: bool,
+        type: str,
+        supported_adapters: list[str],
+        parent=None,
+    ):
+        super().__init__(
+            module_name,
+            project_link,
+            name,
+            desc,
+            author,
+            homepage,
+            tags,
+            is_official,
+            parent=parent,
+        )
+        self.type = type
+        """Plugin category"""
+        self.supported_adapters = supported_adapters
+        """Supported adapters"""
 
 
 class AdapterCard(ExtensionCard):
-    def __init__(self, parent=None):
-        super().__init__(parent=parent)
+    def __init__(
+        self,
+        module_name: str,
+        project_link: str,
+        name: str,
+        desc: str,
+        author: str,
+        homepage: str,
+        tags: list[dict[{"label": str, "color": str}]],
+        is_official: bool,
+        parent=None,
+    ):
+        super().__init__(
+            module_name,
+            project_link,
+            name,
+            desc,
+            author,
+            homepage,
+            tags,
+            is_official,
+            parent=parent,
+        )
