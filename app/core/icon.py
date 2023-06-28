37836877b4ab098657b05238b508234c8239aac0
rainzee wang <rainzee.w@gmail.com>
2023-06-29 05:43:47 +0800

    feat: add new icon

--- app/core/icon.py
+++ app/core/icon.py
@@ -9,6 +9,7 @@ class MyFluentIcon(FluentIconBase, Enum):
     CUT = "Cut"
     COPY = "Copy"
     BOT = "Bot"
+    BOTSPARKLE = "BotSparkle"
 
     def path(self, theme=Theme.AUTO):
         return f"./app/res/icons/{self.value}_{getIconColor(theme)}.svg"
