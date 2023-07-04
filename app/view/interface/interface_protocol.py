324ead8243b60feba46e00c85eac90f5d859a69b
rainzee wang <rainzee.w@gmail.com>
2023-07-05 07:26:38 +0800

    feat: add interface protocol

---
+++ app/view/interface/interface_protocol.py
@@ -0,0 +1,20 @@
+from typing import Protocol, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Optional
+    from PySide6.QtCore import QObject
+
+
+class Interface(Protocol):
+    def __init__(self, parent: Optional[QObject] = None) -> None:
+        # super().__init__(parent)
+        ...
+
+    def __init_widget(self) -> None:
+        ...
+
+    def __init_sub_widget(self) -> None:
+        ...
+
+    def __init_layout(self) -> None:
+        ...
