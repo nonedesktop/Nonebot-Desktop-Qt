df2ea800c76da13893e42d68cdf6d7e8262663e0
rainzee wang <rainzee.w@gmail.com>
2023-07-06 08:56:58 +0800

    feat: add MarketInterface ExtensionCardView

--- app/view/component/__init__.py
+++ app/view/component/__init__.py
@@ -1,1 +1,2 @@
 from .interface_title_bar import InterfaceTitleBar
+from. extension_card import ExtensionCard
--- app/view/interface/market_interface.py
+++ app/view/interface/market_interface.py
@@ -1,22 +1,81 @@
 from PySide6.QtGui import Qt
 from PySide6.QtWidgets import QWidget, QVBoxLayout
-from qfluentwidgets import ScrollArea
+from qfluentwidgets import (
+    ScrollArea,
+    SmoothScrollArea,
+    FlowLayout,
+    SearchLineEdit,
+    PillPushButton,
+    FluentIcon,
+)
 
 from core import StyleSheet
-from view.component import InterfaceTitleBar
+from view.component import InterfaceTitleBar, ExtensionCard
+
+
+class ExtensionCardView(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        # Instantiating widgets
+        self.search_line_edit = SearchLineEdit(self)
+        self.plugin_lable_button = PillPushButton("插件", self, FluentIcon.TAG)
+        self.adapter_lable_button = PillPushButton("适配器", self, FluentIcon.TAG)
+        self.certified_extensions_lable_button = PillPushButton(
+            "官方认证", self, FluentIcon.TAG
+        )
+        self.card_view_container = SmoothScrollArea(self)
+        self.card_view_content = QWidget(self.card_view_container)
+        # Instantiating layouts
+        self.layout_manager = QVBoxLayout(self)
+        self.card_view_content_layout_manager = FlowLayout(
+            self.card_view_content, True, True
+        )
+        # Instantiating data and data structures
+        self.extension_cards: list[ExtensionCard] = []
+        # TODO 实现一个前缀搜索树 Trie Tree
+        self.__init_sub_widget()
+        self.__init_sub_widget()
+        self.__init_layout()
+
+    def __init_sub_widget(self):
+        # init search_line_edit widget
+        self.search_line_edit.setPlaceholderText("搜索插件或适配器")
+        self.search_line_edit.textChanged.connect(self.search_line_edit.search)
+        # init card_view_container widget
+        self.card_view_container.setWidget(self.card_view_content)
+        self.card_view_container.setWidgetResizable(True)
+        self.card_view_container.setHorizontalScrollBarPolicy(
+            Qt.ScrollBarPolicy.ScrollBarAlwaysOff
+        )
+
+    def __init_layout(self):
+        self.layout_manager.setContentsMargins(0, 0, 0, 0)
+        self.layout_manager.setSpacing(10)
+        # TODO 实现一个QFRAM的CommandBar来布局其他按钮
+        self.layout_manager.addWidget(self.search_line_edit)
+        self.layout_manager.addWidget(self.card_view_container)
+
+    def __init_sub_widget_layout(self):
+        # init card_view_container widget layout
+        self.card_view_container.setViewportMargins(2, 5, 2, 5)
+        # init card_view_content widget layout
+        self.card_view_content_layout_manager.setVerticalSpacing
+        self.card_view_content_layout_manager.setHorizontalSpacing
+        self.card_view_content_layout_manager.setContentsMargins
 
 
 class MarketInterface(ScrollArea):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
-        # Instantiating widgets\
+        # Instantiating widgets
+        self.view_container = QWidget(self)
         # TODO 需要微调位置来适配Title Bar
         self.title_bar = InterfaceTitleBar(
             "Extension Store",
             "Enjoy all the extensions from the community, with our rich ecosystem of plugins",
             self,
         )
-        self.view_container = QWidget(self)
+        self.extension_card_view = ExtensionCardView(self)
         # Instantiating layouts
         self.view_container_layout_manager = QVBoxLayout(self.view_container)
         # Initialize self widget & layout
@@ -39,7 +98,8 @@ class MarketInterface(ScrollArea):
         self.setWidget(self.view_container)
         self.view_container_layout_manager.setSpacing(30)
         self.view_container_layout_manager.setAlignment(Qt.AlignmentFlag.AlignTop)
-        self.view_container_layout_manager.setContentsMargins(0, 0, 0, 0)
+        self.view_container_layout_manager.setContentsMargins(36, 20, 36, 36)
+        self.view_container_layout_manager.addWidget(self.extension_card_view)
 
     def resizeEvent(self, event):
         super().resizeEvent(event)
