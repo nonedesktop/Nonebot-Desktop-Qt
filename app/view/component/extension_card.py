266da8f35a07157ec4c4499c6912cb61f86b5e5e
worldmozara <worldmozara@163.com>
2023-07-06 18:14:23 +0800

    refactor: use single ExtensionCard

--- app/view/component/extension_card.py
+++ app/view/component/extension_card.py
@@ -12,6 +12,8 @@ class ExtensionCard(CardWidget):
         homepage: str,
         tags: list[dict[{"label": str, "color": str}]],
         is_official: bool,
+        type: str,
+        supported_adapters: list[str] | None = None,
         parent=None,
     ):
         super().__init__(parent=parent)
@@ -31,62 +33,7 @@ class ExtensionCard(CardWidget):
         """Tags"""
         self.is_official = is_official
         """Whether an extension is official"""
-        
-
-
-class PluginCard(ExtensionCard):
-    def __init__(
-        self,
-        module_name: str,
-        project_link: str,
-        name: str,
-        desc: str,
-        author: str,
-        homepage: str,
-        tags: list[dict[{"label": str, "color": str}]],
-        is_official: bool,
-        type: str,
-        supported_adapters: list[str],
-        parent=None,
-    ):
-        super().__init__(
-            module_name,
-            project_link,
-            name,
-            desc,
-            author,
-            homepage,
-            tags,
-            is_official,
-            parent=parent,
-        )
         self.type = type
         """Plugin category"""
         self.supported_adapters = supported_adapters
         """Supported adapters"""
-
-
-class AdapterCard(ExtensionCard):
-    def __init__(
-        self,
-        module_name: str,
-        project_link: str,
-        name: str,
-        desc: str,
-        author: str,
-        homepage: str,
-        tags: list[dict[{"label": str, "color": str}]],
-        is_official: bool,
-        parent=None,
-    ):
-        super().__init__(
-            module_name,
-            project_link,
-            name,
-            desc,
-            author,
-            homepage,
-            tags,
-            is_official,
-            parent=parent,
-        )
