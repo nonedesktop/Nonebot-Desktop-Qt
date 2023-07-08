9e26bb0575ad5b23586e1c6c2f89fdb0f92d5ff1
rainzee wang <rainzee.w@gmail.com>
2023-07-08 09:31:03 +0800

    fix: adjust InterfaceTitleBar

--- app/view/component/interface_title_bar.py
+++ app/view/component/interface_title_bar.py
@@ -1,14 +1,15 @@
 from PySide6.QtGui import Qt
 from PySide6.QtWidgets import QWidget, QVBoxLayout
-from qfluentwidgets import TitleLabel, CaptionLabel
+
+from .lable import InterfaceTitle, InterfaceDesc
 
 
 class InterfaceTitleBar(QWidget):
     def __init__(self, title: str, subtitle: str, parent=None):
         super().__init__(parent=parent)
         # Instantiating widgets
-        self.title_lable = TitleLabel(title, self)
-        self.subtitle_lable = CaptionLabel(subtitle, self)
+        self.title_lable = InterfaceTitle(title, self)
+        self.subtitle_lable = InterfaceDesc(subtitle, self)
         # Instantiating layouts
         self.layout_manager = QVBoxLayout(self)
         # Initialize self widget & layout
@@ -16,13 +17,10 @@ class InterfaceTitleBar(QWidget):
         self.__init_layout()
 
     def __init_widget(self):
-        self.setFixedHeight(100)
+        self.setFixedHeight(120)
 
     def __init_layout(self):
-        self.layout_manager.setSpacing(0)
         self.setContentsMargins(36, 22, 36, 12)
-        self.layout_manager.addWidget(self.title_lable)
-        self.layout_manager.addSpacing(4)
-        self.layout_manager.addWidget(self.subtitle_lable)
-        self.layout_manager.addSpacing(4)
         self.layout_manager.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.layout_manager.addWidget(self.title_lable)
+        self.layout_manager.addWidget(self.subtitle_lable)
