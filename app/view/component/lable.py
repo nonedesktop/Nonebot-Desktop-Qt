e4cc086a9e081d1de2b886c87d044f2cce618cb0
rainzee wang <rainzee.w@gmail.com>
2023-07-08 09:30:30 +0800

    feat: add lable component

---
+++ app/view/component/lable.py
@@ -0,0 +1,20 @@
+from PySide6.QtGui import QFont
+from qfluentwidgets import FluentLabelBase
+
+
+def getFont(
+    fontSize: int = 14, fontWeight: QFont.Weight = QFont.Weight.Normal) -> QFont:
+    font = QFont()
+    font.setFamilies(["Segoe UI", "Microsoft YaHei", "PingFang SC"])
+    font.setPixelSize(fontSize)
+    font.setWeight(fontWeight)
+    return font
+
+
+class InterfaceTitle(FluentLabelBase):
+    def getFont(self) -> QFont:
+        return getFont(fontSize=28, fontWeight=QFont.Weight.Bold)
+    
+class InterfaceDesc(FluentLabelBase):
+    def getFont(self) -> QFont:
+        return getFont(fontSize=14, fontWeight=QFont.Weight.Normal)
