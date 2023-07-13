c85665365e9020903cdcee4e063e82c1838f3e47
worldmozara <worldmozara@163.com>
2023-07-13 17:51:39 +0800

    fix: correct playground example

--- app/view/component/playground.py
+++ app/view/component/playground.py
@@ -6,15 +6,15 @@ class MainWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
         card = ExtensionCard(
-            "服务器状态查看",
-            "https://github.com/cscs181/QQ-GitHub-Bot/tree/master/src/plugins/nonebot_plugin_status",
+            "nonebot_plugin_status",
+            "nonebot-plugin-status",
             "服务器状态查看",
             "通过戳一戳获取服务器状态",
             "yanyongyu",
             "https://github.com/cscs181/QQ-GitHub-Bot/tree/master/src/plugins/nonebot_plugin_status",
-            ["t:server"],
+            [{"label": "t:server", "color": "#f12020"}],
             True,
-            "Plugin",
+            "application",
             None,
             self
         )
