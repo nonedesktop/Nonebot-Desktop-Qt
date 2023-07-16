63eba71a768d4fad1660b66c4d0a8b597a57a5c7
rainzee wang <rainzee.w@gmail.com>
2023-07-16 15:49:54 +0800

    refactor: use sub layout and clean code

--- app/view/component/extension_card.py
+++ app/view/component/extension_card.py
@@ -1,9 +1,4 @@
-from PySide6.QtWidgets import (
-    QLabel,
-    QHBoxLayout,
-    QVBoxLayout,
-    QSizePolicy
-)
+from PySide6.QtWidgets import QLabel, QHBoxLayout, QVBoxLayout, QSizePolicy
 from PySide6.QtCore import Qt
 from qfluentwidgets import CardWidget, IconWidget, PrimaryPushButton
 from qfluentwidgets import FluentIcon as FI
@@ -28,7 +23,7 @@ class ExtensionCard(CardWidget):
         parent=None,
     ):
         super().__init__(parent=parent)
-
+        # Binding data
         self.module_name = module_name
         """Name for importing"""
         self.project_link = project_link
@@ -49,7 +44,6 @@ class ExtensionCard(CardWidget):
         """Plugin category"""
         self.supported_adapters = supported_adapters
         """Supported adapters"""
-
         # Instantiating widgets
         self.title_label = QLabel(name, self)
         self.content_label = QLabel(self.desc, self)
@@ -62,12 +56,11 @@ class ExtensionCard(CardWidget):
         self.offical_mark_icon = IconWidget(MFI.OFFICALMARK, self)
         self.check_mark_icon = IconWidget(MFI.CHECKPASS, self)
         # TODO MORE WIDGETS
-
         # Instantiating layouts
         self.layout_mannager = QVBoxLayout(self)
+        self.header_bar_layout_manager = QHBoxLayout()
         self.pypi_info_bar_layout_mannager = QHBoxLayout()
         self.author_info_bar_layout_mannager = QHBoxLayout()
-
         # Initialize self widget & layout
         self.__init_sub_widget()
         self.__init_widget()
@@ -75,60 +68,32 @@ class ExtensionCard(CardWidget):
         self.__init_layout()
 
     def __init_widget(self):
+        # Set widgets object name
         self.setObjectName("ExtensionCard")
+        # Apply stylesheet
         StyleSheet.EXTENSION_CARD.apply(self)
 
     def __init_sub_widget(self):
+        # Set widgets object name
         self.title_label.setObjectName("ExtensionCardTitleLabel")
         self.content_label.setObjectName("ExtensionCardContentLabel")
         self.author_label.setObjectName("ExtensionCardAuthorLabel")
         self.pypi_label.setObjectName("ExtensionCardPypiLabel")
-
+        # Set widgets options
         self.content_label.setWordWrap(True)
-        # title_font = self.title_label.font()
-        # title_font.setPointSize(12)
-        # self.title_label.setFont(title_font)
-        # self.title_label.setToolTip("非官方插件")
-        # if self.is_official:
-        #     self.title_label.setText(
-        #         "<html><head/><body><p><span style=\" color:#0cd50c;\">"
-        #         f"{self.name}</span></p></body></html>"
-        #     )
-        #     self.title_label.setToolTip("官方插件")
-
-        # self.pypi_label.setToolTip(self.module_name)
-
-        # Here we depens on the status to modfiy the button's text:
-        # if installed set uninstall
-        # if uninstalled set install
-        # we get the status from instance globe status object
-
-        # self.mannage_button.setText()
 
     def __init_layout(self):
+        # Set layout options
         self.setFixedSize(360, 168)
         self.layout_mannager.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
         self.layout_mannager.setContentsMargins(16, 12, 12, 12)
-
-        self.layout_mannager.addWidget(self.title_label)
+        # Place layout widgets
+        self.layout_mannager.addLayout(self.header_bar_layout_manager)
         self.layout_mannager.addWidget(self.content_label)
         self.layout_mannager.addLayout(self.pypi_info_bar_layout_mannager)
         self.layout_mannager.addLayout(self.author_info_bar_layout_mannager)
         self.github_icon.move(320, 128)
         # self.mannage_button.move(220, 128)  # 临时放在这里
-        self.offical_mark_icon.move(self.title_label.x() + self.title_label.width() + 45, self.title_label.y() + 13)
-        self.check_mark_icon.move(self.offical_mark_icon.x() + 25, self.offical_mark_icon.y())
-
-        # vbox layout:
-        #     label title
-        #     textbrowser content
-        #     hbox pypi_info:
-        #         icon pypi_icon
-        #         label pypi_name
-        #     hbox author_info:
-        #         icon author_icon
-        #         label author_name
-        # icon gh_icon
 
     def __init_sub_widget_layout(self):
         self.offical_mark_icon.setFixedSize(22, 22)
@@ -137,15 +102,17 @@ class ExtensionCard(CardWidget):
         self.pypi_icon.setFixedSize(14, 14)
         self.author_icon.setFixedSize(14, 14)
         self.github_icon.setFixedSize(28, 28)
-
+        # Place header bar layout widgets
+        self.header_bar_layout_manager.addWidget(self.title_label)
+        self.header_bar_layout_manager.addWidget(self.offical_mark_icon)
+        self.header_bar_layout_manager.addWidget(self.check_mark_icon)
+        # Place pypi bar layout widgets
         self.pypi_info_bar_layout_mannager.setAlignment(Qt.AlignmentFlag.AlignVCenter)
         self.pypi_info_bar_layout_mannager.addWidget(self.pypi_icon)
         self.pypi_info_bar_layout_mannager.addSpacing(2)
         self.pypi_info_bar_layout_mannager.addWidget(self.pypi_label)
-
+        # Place author bar layout widgets
         self.author_info_bar_layout_mannager.setAlignment(Qt.AlignmentFlag.AlignVCenter)
         self.author_info_bar_layout_mannager.addWidget(self.author_icon)
         self.author_info_bar_layout_mannager.addSpacing(2)
         self.author_info_bar_layout_mannager.addWidget(self.author_label)
-
-    # TODO: Click vbox to perform installation (ask users)
