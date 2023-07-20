ed9277c79af05e61fb20209c31ddc9675975f308
rainzee wang <rainzee.w@gmail.com>
2023-07-20 16:49:28 +0800

    improvement: add action button and status stuff

--- app/view/component/instance_card.py
+++ app/view/component/instance_card.py
@@ -1,4 +1,4 @@
-from qfluentwidgets import CardWidget, IconWidget
+from qfluentwidgets import CardWidget, InfoBadge, DotInfoBadge, TransparentToolButton
 from qfluentwidgets import FluentIcon as FI
 from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QLabel, QHBoxLayout, QVBoxLayout
@@ -10,14 +10,15 @@ class InstanceCard(CardWidget):
         # Instantiating Data Structures
         # Instantiating Widget Objects
         self.instance_name_label = QLabel(instance_name, self)
-        self.instance_status_icon = IconWidget(FI.FLAG, self)  # 挑选一个简单的圆点图标用不同颜色来表示实例的运行状态
+        self.instance_status_label = QLabel("运行中", self)
+        self.instance_status_icon = DotInfoBadge.success(self)
         self.instance_id_label = QLabel(instance_id, self)
-        self.instance_id_icon = IconWidget(FI.FINGERPRINT, self)
+        self.instance_id_icon = InfoBadge.info("实例ID")
         self.driver_name_label = QLabel(driver_name, self)
-        self.driver_icon = IconWidget(FI.CALORIES, self)
+        self.driver_icon = InfoBadge.info("驱动器")
         self.adapter_name_label = QLabel(adapter_name, self)
-        self.adapter_icon = IconWidget(FI.AIRPLANE, self)
-        # TODO Add Butoon Or Other Widgets
+        self.adapter_icon = InfoBadge.info("适配器")
+        self.more_action_button = TransparentToolButton(FI.MORE, self)
         # Instantiating Layout Objects
         self.layout_manager = QVBoxLayout(self)
         self.header_bar_layout_manager = QHBoxLayout()
@@ -32,33 +33,40 @@ class InstanceCard(CardWidget):
         # Set Object Name
         self.setObjectName("InstanceCard")
         # Set Widget Option & Policy
+        self.setFixedHeight(80)
+        # Set Geometry
+
         pass
 
     def __init_sub_widget(self) -> None:
         # Set Object Name
         # Set Widget Option & Policy
-        pass
+        # Set Geometry
+        self.instance_status_icon.setFixedSize(6, 6)
 
     def __init_layout(self) -> None:
         # Set Layout Options
-        self.setFixedHeight(100)
         self.layout_manager.setAlignment(Qt.AlignmentFlag.AlignTop)
         # Place layout widgets
         self.layout_manager.addLayout(self.header_bar_layout_manager)
         self.layout_manager.addLayout(self.info_bar_layout_manager)
 
     def __init_sub_widget_layout(self) -> None:
+        # Set Layout Options
+        # self.info_bar_layout_manager.setSizeConstraint(QHBoxLayout.SizeConstraint.SetMinimumSize)
         # Place header bar layout widgets
         self.header_bar_layout_manager.addWidget(self.instance_name_label, alignment=Qt.AlignmentFlag.AlignLeft)
+        self.header_bar_layout_manager.addStretch(1)
         self.header_bar_layout_manager.addWidget(self.instance_status_icon, alignment=Qt.AlignmentFlag.AlignRight)
-        self.header_bar_layout_manager.addWidget(self.instance_name_label, alignment=Qt.AlignmentFlag.AlignRight)
+        self.header_bar_layout_manager.addWidget(self.instance_status_label, alignment=Qt.AlignmentFlag.AlignRight)
         # Place info bar layout widgets
         self.info_bar_layout_manager.addWidget(self.instance_id_icon)
         self.info_bar_layout_manager.addWidget(self.instance_id_label)
-        # TODO Add Space Here
+        self.info_bar_layout_manager.addSpacing(5)
         self.info_bar_layout_manager.addWidget(self.driver_icon)
         self.info_bar_layout_manager.addWidget(self.driver_name_label)
-        # TODO Add Space Here
+        self.info_bar_layout_manager.addSpacing(5)
         self.info_bar_layout_manager.addWidget(self.adapter_icon)
         self.info_bar_layout_manager.addWidget(self.adapter_name_label)
-        pass
+        self.info_bar_layout_manager.addStretch(1)
+        self.info_bar_layout_manager.addWidget(self.more_action_button)
