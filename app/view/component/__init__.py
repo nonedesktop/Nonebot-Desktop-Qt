e8c046c71848f50589bd7430ddff1cce7b341103
rainzee wang <rainzee.w@gmail.com>
2023-07-22 15:43:14 +0800

    improvement: Initial label

--- app/view/component/__init__.py
+++ app/view/component/__init__.py
@@ -1,6 +1,28 @@
 from .interface_title_bar import InterfaceTitleBar
 from .extension_card import ExtensionCard
 from .instance_card import InstanceCard
+from .label import (
+    CaptionLabel,
+    BodyLabel,
+    BodyStrongLabel,
+    BodyLargeLabel,
+    SubtitleLabel,
+    TitleLabel,
+    TitleLargeLabel,
+    DisplayLabel,
+)
 
 
-__all__: list[str] = ["InterfaceTitleBar", "ExtensionCard", "InstanceCard"]
+__all__: list[str] = [
+    "InterfaceTitleBar",
+    "ExtensionCard",
+    "InstanceCard",
+    "CaptionLabel",
+    "BodyLabel",
+    "BodyStrongLabel",
+    "BodyLargeLabel",
+    "SubtitleLabel",
+    "TitleLabel",
+    "TitleLargeLabel",
+    "DisplayLabel",
+]
