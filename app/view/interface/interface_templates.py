f6f6d9f3d375516de65146417fb6ab718d6cb782
rainzee wang <rainzee.w@gmail.com>
2023-09-02 05:05:03 +0800

    feat: add widget templates

--- app/view/interface/interface_templates.py
+++ app/view/interface/interface_templates.py
@@ -1,4 +1,5 @@
-from PySide6.QtCore import Qt
+from typing import Optional
+from PySide6.QtCore import Qt, QObject, Signal
 from PySide6.QtWidgets import QFrame, QHBoxLayout
 from qfluentwidgets import SubtitleLabel, setFont
 
@@ -13,3 +14,54 @@ class InterfaceTemplates(QFrame):
         self.sub_title.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.layout_manager.addWidget(self.sub_title, 1, Qt.AlignmentFlag.AlignCenter)
         self.layout_manager.setContentsMargins(0, 32, 0, 0)
+
+
+class Templates(QObject):
+    """Widget Doc"""
+
+    someSignal: Signal = Signal()
+    """Signal Doc"""
+
+    def __init__(self, parent: Optional[QObject] = None) -> None:
+        super().__init__(parent=parent)
+        # Initializing Data Bindings and Objects
+        # Instantiating Widget Objects
+        # Instantiating Layout Objects
+        # Initializing Widget & Layout
+        self.__init_sub_widget__()
+        self.__init_widget__()
+        self.__init_sub_layout__()
+        self.__init_layout__()
+
+    def __init_widget__(self) -> None:
+        # Setting Objects Name
+        # Setting Widget Options
+        # Setting Widget Sub Widget
+        # Setting Widget Geometry
+        # Setting Widget StyleSheet
+        pass
+
+    def __init_sub_widget__(self) -> None:
+        # Setting Objects Name
+        # Setting Widget Options
+        # Setting Widget Sub Widget
+        # Setting Widget Geometry
+        # Setting Widget StyleSheet
+        pass
+
+    def __init_layout__(self) -> None:
+        # Setting Layout Options
+        # Arranging Widgets
+        pass
+
+    def __init_sub_layout__(self) -> None:
+        # Setting Layout Options
+        # Arranging Widgets
+        pass
+
+    def event_rewrite(self) -> None:
+        pass
+
+    def public_api(self) -> None:
+        """API Doc"""
+        pass
